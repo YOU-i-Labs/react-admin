13f955b522dbcbfac0fa4df6b84ded0f044b044e
Laura Alkhoury <laura.alkhoury@youi.tv>
2019-10-08 09:34:15 -0400

    Fix import

--- examples/youi-analytics/dashboard/src/routes/Activity/Activity.js
+++ examples/youi-analytics/dashboard/src/routes/Activity/Activity.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import QueryExecutor from '../../components/DataQuery/QueryExecutor';
-import PieChart from '../../components/Charts/PieChart'
+import PieChart from '../../components/NivoCharts/PieChart'
 import * as Constants from '../../components/DataQuery/Queries'
 import { Title } from 'react-admin'; 
 
